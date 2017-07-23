04d2db56f9ddaaf02f54790294f6a8e76385e075
Watson <wkwats@gmail.com>
2017-07-23 22:30:54 +0300

    Change root based on user signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   get 'welcome/index'
+
+  
+  resources :notes
+
+  authenticated :user do
+
+  	root 'notes#index',  as: "authenticated_root"
+
+  end
+
+
   
   root 'welcome#index'
 
-  resources :notes
+  
 
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
